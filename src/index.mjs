a0c0956d337e675a930984b57ca432d1bd153892
programmernb-ctrl <141086417+programmernb-ctrl@users.noreply.github.com>
2025-05-19 23:17:49 +0200

    updated to v0.0.2

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,6 @@
 /** @format */
-
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
-import axios from 'axios';
-
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 import { LOGGER } from './logger.mjs';
 
 dotenv.config();
@@ -16,20 +12,40 @@ const BASE_URL = 'https://api.github.com';
 
 async function updateJSON() {
 	try {
-		const response = await axios.get(`${BASE_URL}/repos/${REPO_OWNER}/${REPO_NAME}/commits`, {
+		const response = await fetch(`${BASE_URL}/repos/${REPO_OWNER}/${REPO_NAME}/commits`, {
 			headers: {
 				'X-GitHub-Api-Version': '2022-11-28',
 				Accept: 'application/vnd.github+json',
 			},
 		});
 
-		const lastCommit = response.data[0];
+		if (!response.ok) {
+			return;
+		}
+
+		const commits = await response.json();
+		const lastCommit = commits[0];
+
+		if (!lastCommit) {
+			LOGGER.warn("No Commits found in the repository.");
+			return;
+		}
+
 		const commitInfo = {
 			sha: lastCommit.sha,
 			updatedAt: lastCommit.commit.author.date,
+			commit: lastCommit.commit.message,
+			tree: lastCommit.commit.tree
 		};
 
-		const data = JSON.parse(fs.readFileSync('dist/data.json', 'utf-8')); // Ensuring it's never null
+		if (!fs.existsSync('dist')) {
+			fs.mkdirSync('dist');
+		}
+
+		let data = {};
+		if (fs.existsSync('dist/data.json')) {
+			data = JSON.parse(fs.readFileSync('dist/data.json', 'utf-8'));
+		}
 
 		data.data = commitInfo;
 
